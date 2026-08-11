bae528cad4075abec8f614d6f90ba60ca6c5d9fc
viniciusgabrielos <viniciusgabriel3011@gmail.com>
2026-08-11 02:25:20 +0000

    Adiciona busca de trilhas por dificuldade

--- vinicius.py
+++ vinicius.py
@@ -1,3 +1,62 @@
+ # Banco de dados provisório para testar as funções
+
+trilhas = {
+    "101": {
+        "id_trilha": 101,
+        "nome": "Trilha A",
+        "local": "Crato - CE",
+        "dificuldade": "Fácil",
+        "capacidade": 10,
+        "preco": 50.00,
+        "status": "Disponível",
+        "inscritos": []
+    },
+
+    "102": {
+        "id_trilha": 102,
+        "nome": "Trilha B",
+        "local": "Barbalha - CE",
+        "dificuldade": "Média",
+        "capacidade": 15,
+        "preco": 70.00,
+        "status": "Disponível",
+        "inscritos": []
+    },
+
+    "103": {
+        "id_trilha": 103,
+        "nome": "Trilha C",
+        "local": "Juazeiro do Norte - CE",
+        "dificuldade": "Difícil",
+        "capacidade": 8,
+        "preco": 90.00,
+        "status": "Disponível",
+        "inscritos": []
+    },
+
+    "104": {
+        "id_trilha": 104,
+        "nome": "Trilha D",
+        "local": "Crato - CE",
+        "dificuldade": "Fácil",
+        "capacidade": 20,
+        "preco": 40.00,
+        "status": "Disponível",
+        "inscritos": []
+    },
+
+    "105": {
+        "id_trilha": 105,
+        "nome": "Trilha E",
+        "local": "Barbalha - CE",
+        "dificuldade": "Difícil",
+        "capacidade": 12,
+        "preco": 100.00,
+        "status": "Disponível",
+        "inscritos": []
+    }
+}
+
 # Função responsável por classificar o nível do trilheiro
 # com base na quantidade de trilhas que ele já concluiu.
 def classificar_nivel_trilheiro(trilhas_concluidas):
@@ -26,3 +85,13 @@ def classificar_nivel_trilheiro(trilhas_concluidas):
     # significa que o usuário concluiu mais de 20 trilhas.
     else:
         return "Lenda das Trilhas"
+
+# função para mostrar na tela as listas que ele deseja por nível de dificuldade
+def buscar_por_dificuldade(nivel_desejado):
+    encontradas = []
+    for trilha in trilhas:
+        if trilhas[trilha]["dificuldade"] == nivel_desejado:
+            encontradas.append(trilhas[trilha]["nome"])
+    return encontradas
+
+
