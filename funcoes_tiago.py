2390ad6bf6513cd39a8ae4e242bbf18e4c97fcfc
Tiago Pereira da Silva <seu_email_do_github@exemplo.com>
2026-08-12 22:51:57 -0300

    Lê o conteúdo do arquivo e retorna um dicionário mapeado ou cria e retorna estruturas base caso haja erro.

--- funcoes_tiago.py
+++ funcoes_tiago.py
@@ -1,17 +1,17 @@
 import json
 
+
 def carregar_dados_sistema(nome_arquivo_json):
     try:
         # Abre o arquivo em modo leitura ("r") com suporte a acentos (utf-8).
         with open(nome_arquivo_json, "r", encoding="utf-8") as banco_de_dados:
             # Lê o conteúdo do arquivo e retorna um dicionário mapeado.
             return json.load(banco_de_dados)
-            
+
     except FileNotFoundError:
         # Cria e retorna um dicionário base estrutural vazio caso o arquivo não exista.
         print("Aviso: Banco de dados não encontrado. Iniciando um novo sistema vazio.")
         return {"trilhas": {}, "participantes": {}, "guias": {}}
-
 
     except json.JSONDecodeError:
         # Cria e retorna um dicionário base estrutural vazio caso o arquivo exista, mas possua o conteúdo corrompido ou escrito
@@ -19,4 +19,3 @@ def carregar_dados_sistema(nome_arquivo_json):
         print("ERRO CRÍTICO: O arquivo do banco de dados está corrompido!")
         print("Iniciando um sistema vazio por segurança. Verifique o arquivo .json.")
         return {"trilhas": {}, "participantes": {}, "guias": {}}
-        
