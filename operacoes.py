eb1db6f5396d64173a32994e90ddb2d528e9b69e
tiagopds-dev <tiagopereiradasilva100@gmail.com>
2026-08-15 21:06:36 -0300

    Adiciona a função "gerar_relatorio_geral" no módulo correto.

--- operacoes.py
+++ operacoes.py
@@ -31,3 +31,25 @@ def adicionar_participante (id_trilha, novo_participante):
     for participante in trilha["inscritos"]:
         if participante["id_participante"] == novo_participante["id_participante"]:
             return "Participante já inscrito na trilha."
+
+
+
+def gerar_relatorio_geral(lista_trilhas):
+    lucro_total = 0
+
+    print("=== Relatório Geral de Trilhas ===")
+    for id_trilha in lista_trilhas:
+        trilha = lista_trilhas[id_trilha]
+
+        # Calcula ocupação em porcentagem
+        vagas_ocupadas = len(trilha["inscritos"])
+        capacidade = trilha["capacidade"]
+        ocupacao_percentual = (vagas_ocupadas / capacidade) * 100
+
+        # Chama a função calcular_faturamento_trilha
+        faturamento = calcular_faturamento_trilha(id_trilha)
+        lucro_total += faturamento
+
+        print(f"{trilha['nome']}: {ocupacao_percentual:.1f}% ocupada | Faturamento: R${faturamento:.2f}")
+
+    print(f"\nLucro total do guia: R${lucro_total:.2f}")
